ef95c26c6db69645ae8cbb29efc689941fdaf064
Matteo DeSimone <thematteod@gmail.com>
2022-08-17 14:52:44 -0400

    Initial commit

---
+++ VariablePracticeConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿//Declare Variables
+int age;
+string name;
+bool isAlive;
+char lastInitial;
+decimal weight;
+double siblings;
+
+//Initialize Variables
+age = 24;
+name = "Matteo";
+isAlive = true;
+lastInitial = 'D';
+weight = 153.2m;
+siblings = 1;
+
+
+//Print Variables
+Console.WriteLine($"My name is {name} {lastInitial}! I am {age} years old with {siblings} sibling(s). I weigh {weight} pounds and am currently alive: {isAlive}");
